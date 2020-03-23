1347a47dbd979fd765bc5cfa3cfa713ec6035c93
Steven Ortiz <sortiz832@gmail.com>
2020-03-22 20:42:44 -0500

    Added hook

---
+++ hooks/splashscreen-theme.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const PATH = './platforms/android/app/src/main/java/org/apache/cordova/splashscreen/SplashScreen.java';
+const OLD_TEXT = 'android.R.style.Theme_Translucent_NoTitleBar';
+const NEW_TEXT = 'cordova.getActivity().getResources().getIdentifier("AppTheme", "style", cordova.getActivity().getPackageName())';
+
+module.exports = function() {
+  try {
+    fs.writeFileSync(PATH, fs.readFileSync(PATH, 'utf-8').replace(OLD_TEXT, NEW_TEXT));
+  } catch(exc) {
+    console.error(exc);
+  }
+}
