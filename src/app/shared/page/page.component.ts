a05b466f33bc16b8f5add64f6b4663267103572d
Steven Ortiz <sortiz832@gmail.com>
2022-08-31 15:49:43 -0500

    Refactored assets and pages

--- src/app/shared/page/page.component.ts
+++ src/app/shared/page/page.component.ts
@@ -1,10 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.scss'],
 })
-export class PageComponent {
-  @Input() title: string;
+export class PageComponent implements OnInit {
+  @Input()
+  title: string;
+
+  constructor(private element: ElementRef<HTMLElement>) {
+  }
+
+  ngOnInit(): void {
+    const source = this.element.nativeElement;
+    const target = source.parentElement;
+
+    while (source.firstChild) {
+      target.insertBefore(source.firstChild, source);
+    }
+
+    source.remove();
+  }
 }
